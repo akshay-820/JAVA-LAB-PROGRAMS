35498c10152133314adb32846e1fd2f12777e00e
akshay-820 <akshay200583@gmail.com>
2025-05-19 12:03:45 +0530

    Lab-3a Program

---
+++ Lab3a.java
@@ -0,0 +1,25 @@
+public class TextProcessing {
+
+    public static boolean compare(String str1, String str2) {
+        return str1.equals(str2);
+    }
+
+    public static String concat(String str1, String str2) {
+        return str1 + str2;
+    }
+
+    public static String copy(String str1) {
+        return new String(str1);
+    }
+
+    public static void main(String[] args) {
+        String str1 = "cool";
+        String str2 = "Cool";
+        System.out.println("Are string 1 and string 2 equal? " + compare(str1, str2));
+        System.out.println("New string after concatenation: " + concat(str1, str2));
+        String original = "HELLO";
+        String copy = copy(original);
+        System.out.println("Original string: " + original);
+        System.out.println("Copy of string: " + copy);
+    }
+}
