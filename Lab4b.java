55b812ffa3471539a1d49758b7e17d6a9a649e29
akshay-820 <akshay200583@gmail.com>
2025-05-19 12:10:57 +0530

    Lab-4b Program

---
+++ Lab4b.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class tracker{
+    public static void main(String[] args){
+        int n=5;
+        Scanner sc= new Scanner(System.in);
+        int[] scores= new int[n];
+        System.out.println("Enter scores of student");
+        for(int i=0;i<n;i++){
+            System.out.print("Enter score of student"+(i+1)+" : ");
+                scores[i]=sc.nextInt();
+        }
+        int sum=0;
+        int maxScore=scores[0];
+        for(int i=0;i<n;i++){
+            sum+=scores[i];
+            if (scores[i]>maxScore){
+                maxScore=scores[i];
+            }
+        }    
+        System.out.println("Stats is ");
+        System.out.println("SUM OF SCORES : "+sum);
+        System.out.println("Highest score : "+maxScore);
+    }   
+}
