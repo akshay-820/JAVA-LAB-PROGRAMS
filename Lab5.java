a61886c359daad895fefe74967d671f455811c11
akshay-820 <akshay200583@gmail.com>
2025-05-19 12:12:16 +0530

    Lab5 Program

---
+++ Lab5.java
@@ -0,0 +1,47 @@
+import java.util.Vector;
+
+public class OnlineShoppingManager {
+    Vector<Product> list = new Vector<>();
+
+    public void addEle(String name, int quantity, String type) {
+        Product p = new Product(name, quantity, type);
+        list.add(p);
+    }
+
+    public void remove(String name) {
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).name.equals(name)) {
+                list.remove(i);
+                break;
+            }
+        }
+    }
+
+    public void display() {
+        System.out.println("Inventory:");
+        for (Product product : list) {
+            System.out.println("Name: " + product.name + " | Quantity: " + product.quantity + " | Type: " + product.type);
+        }
+    }
+
+    public static void main(String[] args) {
+        OnlineShoppingManager manager = new OnlineShoppingManager();
+        manager.addEle("milk", 8, "dairy");
+        manager.addEle("aata", 5, "grocery");
+        manager.display();
+        manager.remove("milk");
+        manager.display();
+    }
+}
+
+class Product {
+    String name;
+    int quantity;
+    String type;
+
+    Product(String name, int quantity, String type) {
+        this.name = name;
+        this.quantity = quantity;
+        this.type = type;
+    }
+}
