4a4cc03cbf546a940c0ca6d40c8688535cadc58d
akshay-820 <akshay200583@gmail.com>
2025-05-19 12:32:15 +0530

    Lab-3b Program

--- 3bstring_buffer.java
+++ 3bstring_buffer.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class string_buffer {
+class 3bstring_buffer {
     public static void main(String[] args) {
         StringBuffer sb1= new StringBuffer();
         StringBuffer sb2= new StringBuffer("Hello");
